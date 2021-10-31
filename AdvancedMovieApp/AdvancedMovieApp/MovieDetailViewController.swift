47a9a978a447303119cfc53fb8f288aafbe6509b
Gizem Boskan <gizem.boskan.dev@gmail.com>
2021-10-31 14:01:49 +0300

    Bind MovieDetailView by MovieDetailViewModel and show in MovieDetailViewController

--- AdvancedMovieApp/AdvancedMovieApp/MovieDetailViewController.swift
+++ AdvancedMovieApp/AdvancedMovieApp/MovieDetailViewController.swift
@@ -6,12 +6,121 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MovieDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    // MARK: - Properties
+    private let bag = DisposeBag()
+    private var movieDetailView = MovieDetailView()
+    var viewmodel = MovieDetailViewModel()
+    
+    // MARK: - Initilizations
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        arrangeViews()
+        observeDataSource()
+        movieDetailView.castCollectionView.reloadData()
+    }
+    
+}
+//  MARK: - Arrange Views
+extension MovieDetailViewController {
+    
+    func arrangeViews() {
+        view = movieDetailView
+        movieDetailView.castCollectionView.delegate = self
+        movieDetailView.castCollectionView.dataSource = self
+    }
+}
+
+// MARK: - Observe Data Source
+extension MovieDetailViewController {
+    
+    func observeDataSource(){
+        
+        viewmodel.movieDetailDatasource.subscribe(onNext: { [weak self] data in
+            guard let self = self,
+                  let movie = self.viewmodel.movieDetailDatasource.value else { return }
+            self.observeUI(with: movie)
+            self.viewmodel.getMovieCredits(movieId: movie.id)
+        }).disposed(by: bag)
+        
+        viewmodel.movieCreditsDatasource.subscribe(onNext: { [weak self] data in
+            guard let self = self else { return }
+            print(data.count)
+            self.movieDetailView.castCollectionView.reloadData()
+        }).disposed(by: bag)
+    }
+    
+    func observeUI(with movie: Movie?) {
+        
+        let view = movieDetailView
+        let movieTitle = movie?.title
+        title = movieTitle
+        let posterPath = movie?.posterPath
+        let foregroundPosterPath = movie?.backdropPath
+        let posterImageViewURL = URL.posterImage(posterPath: posterPath.orEmpty)
+        let foregroundPosterImageViewURL = URL.posterImage(posterPath: foregroundPosterPath.orEmpty)
+        let releaseDate = movie?.releaseDate.orEmpty
+        let rating = movie?.voteAverage ?? 0.0
+        let movieDescription = movie?.overview ?? ""
+        // let movieId = movie.id
+        view.populateUI(posterImageViewURL: posterImageViewURL, foregroundPosterImageViewURL: foregroundPosterImageViewURL, movieTitle: movieTitle ?? "", releaseDate: releaseDate ?? "", rating: rating, movieDescription: movieDescription)
+    }
+}
+// MARK: - UICollectionViewDataSource
+extension MovieDetailViewController: UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewmodel.movieCreditsDatasource.value.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: MovieDetailCollectionViewCell = collectionView.dequeue(at: indexPath)
+        let movieCredits = self.viewmodel.movieCreditsDatasource.value[indexPath.item]
+        let profilePath = movieCredits.profilePath.orEmpty
+        let castMemberImageViewURL = URL.posterImage(posterPath: profilePath)
+        let castMemberCategory = movieCredits.job.orEmpty
+        let castMemberName = movieCredits.originalName
+        cell.populateUI(castMemberImageViewURL: castMemberImageViewURL, castMemberCategory: castMemberCategory, castMemberName: castMemberName)
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension MovieDetailViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let movieCredits = self.viewmodel.movieCreditsDatasource.value[indexPath.item]
+        presentPersonDetail(with: movieCredits.id)
+    }
+    
+}
+
+// MARK: - Person detail
+private extension MovieDetailViewController {
+    func presentPersonDetail(with id: Int?) {
+        
+        guard let viewController = PersonDetailViewController() as? PersonDetailViewController else {
+            assertionFailure("PersonDetailViewController not found")
+            return
+        }
+        viewController.viewmodel.personIdDatasource.accept(id)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 }
--- AdvancedMovieApp/AdvancedMovieApp/MovieDetailViewModel.swift
+++ AdvancedMovieApp/AdvancedMovieApp/MovieDetailViewModel.swift
@@ -9,49 +9,61 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-struct MovieDetailListViewModel {
-    let movieDetailsVM: [MovieDetailViewModel]
-}
+final class MovieDetailViewModel {
+    
+    // MARK: - Properties
+    var isLoading = BehaviorRelay<Bool>(value: false)
+    private(set) var movieDetailDatasource = BehaviorRelay<Movie?>(value: nil)
+    private(set) var movieCreditsDatasource = BehaviorRelay<[Cast]>(value: [])
+    private(set) var bag = DisposeBag()
+    var id: Int = 0
 
-extension MovieDetailListViewModel{
-    init(_ movieDetails: [MovieDetail]) {
-        movieDetailsVM = movieDetails.compactMap(MovieDetailViewModel.init)
+    
+    func updateLoading(){
+        isLoading.accept(false)
+    }
+    func getMovieCredits(movieId: Int) {
+        
+        Observable.just((id))
+            .do(onNext: { [isLoading] _ in isLoading.accept(true) })
+        guard let url = URL.getMovieCredits(id: movieId) else { return }
+        URLRequest.load(resource: Resource<CastResults>(url: url))
+            .observe(on: MainScheduler.instance)
+            .do(onDispose: { [isLoading] in isLoading.accept(false) })
+            .subscribe(onNext: { [weak self] movieCreditsResponse in
+                let movieCredits = movieCreditsResponse.cast
+                self?.updateMovieCreditsDatasource(with: movieCredits)
+                
+                print(self?.movieCreditsDatasource.value as Any)
+            })
+            .disposed(by: bag)
     }
 }
 
-extension MovieDetailListViewModel {
-    
-    func movieDetailsAt( _ index: Int) -> MovieDetailViewModel {
-        movieDetailsVM[index]
-    }
-    
-}
-
-struct MovieDetailViewModel {
-    let movieDetail: MovieDetail
-    
-    init(_ movieDetail: MovieDetail) {
-        self.movieDetail = movieDetail
-    }
-}
-
+//MARK: - Helper Methods
 extension MovieDetailViewModel {
+    
+    func updateMovieCreditsDatasource(with movieCredits: [Cast]) {
+        
+        self.movieCreditsDatasource.accept(movieCredits)
+    }
+    
     
     // TODO: ADD VIDEO AND CAST MEMBERS!!
     
-    var title: Observable<String> {
-        Observable<String>.just(movieDetail.title ?? "")
-    }
-    
-    var description: Observable<String> {
-        Observable<String>.just(movieDetail.overview ?? "")
-    }
-    
-    var poster: Observable<String> {
-        Observable<String>.just(movieDetail.posterPath ?? "")
-    }
-    
-    var averageVote: Observable<Double> {
-        Observable<Double>.just(movieDetail.voteAverage ?? 0.0)
-    }
+    //    var title: Observable<String> {
+    //        Observable<String>.just(movieDetail.title ?? "")
+    //    }
+    //
+    //    var description: Observable<String> {
+    //        Observable<String>.just(movieDetail.overview ?? "")
+    //    }
+    //
+    //    var poster: Observable<String> {
+    //        Observable<String>.just(movieDetail.posterPath ?? "")
+    //    }
+    //
+    //    var averageVote: Observable<Double> {
+    //        Observable<Double>.just(movieDetail.voteAverage ?? 0.0)
+    //    }
 }
